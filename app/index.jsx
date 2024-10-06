a2542a31674f037d03062b7bef0d0cb290b1fe1b
nadira1187 <nadira35-1187@s.diu.edu.bd>
2024-10-06 15:47:01 +0600

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white" >
+      <Text className="text-3xl font-pblack">Lumeo</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{color:'blue'}}>Go to Home</Link>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
